2e79d30150c2ed432e152b83b7a51eb00ec51e4a
MarkDemidovs <markdemidovs@proton.me>
2026-03-02 23:40:23 +0200

    complete deleteBookmark route

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUsers, register, login, createBookmark } from "../controllers/users";
+import { getUsers, register, login, createBookmark, deleteBookmark } from "../controllers/users";
 import { requireAuth } from "../middleware/auth";
 
 const router = Router();
@@ -8,5 +8,6 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/", requireAuth, getUsers); 
 router.post("/bookmarks", requireAuth, createBookmark);
+router.delete("/bookmarks/:id", requireAuth, deleteBookmark);
 
 export default router;
