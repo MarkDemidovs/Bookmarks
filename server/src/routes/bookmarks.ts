582165fa87581a1f47e9efab425e21fc07591f3d
MarkDemidovs <markdemidovs@proton.me>
2026-03-03 16:45:29 +0200

    route getAllBookmarks

--- server/src/routes/bookmarks.ts
+++ server/src/routes/bookmarks.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createBookmark, deleteBookmark } from "../controllers/bookmarks";
+import { createBookmark, deleteBookmark, getAllBookmarks } from "../controllers/bookmarks";
 import { requireAuth } from "../middleware/auth";
 
 const router = Router();
 
 router.post("/bookmarks", requireAuth, createBookmark);
 router.delete("/bookmarks/:id", requireAuth, deleteBookmark);
+router.get("/bookmarks", requireAuth, getAllBookmarks);
 
 export default router;
