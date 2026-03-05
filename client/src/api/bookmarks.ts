558607d0f7e3d3586a6e862fa23be0f4868061da
MarkDemidovs <markdemidovs@proton.me>
2026-03-05 16:44:58 +0200

    api bookmarks.ts

---
+++ client/src/api/bookmarks.ts
@@ -0,0 +1,25 @@
+import type { bookmarkType } from "../types/bookmark";
+
+async function apiFetch<T>(
+  url: string,
+  options?: RequestInit
+): Promise<T> {
+
+  const res = await fetch(`http://localhost:4000${url}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(options?.headers || {})
+    },
+    ...options
+  });
+
+  if (!res.ok) {
+    throw new Error("API error");
+  }
+
+  return res.json();
+}
+
+export function getAllBookmarks(): Promise<bookmarkType[]> {
+    return apiFetch<bookmarkType[]>("/bookmarks")
+}
