d89ac1e1d4e83567e8efd759a40a1b81ce6a2323
MarkDemidovs <markdemidovs@proton.me>
2026-03-05 16:50:30 +0200

    client added react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import type { bookmarkType } from "./types/bookmark";
+import { getAllBookmarks } from "./api/bookmarks";
+
 export default function App() {
-  return (1)
+  
 }
