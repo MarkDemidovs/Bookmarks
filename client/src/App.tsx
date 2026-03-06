693f3b95c10bc7d46e386e4eb015fdd3476fda66
MarkDemidovs <markdemidovs@proton.me>
2026-03-06 23:24:40 +0200

    client added Register.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/register" element={<Register />}/>
       </Routes>
     </BrowserRouter>
   );
