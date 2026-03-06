1e47ed5f08a71a8758d5e6e9f7e903bca09ff514
MarkDemidovs <markdemidovs@proton.me>
2026-03-06 23:33:19 +0200

    client register completed

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <Link to="/login">Go to Login</Link>
+    <>
+      <Link to="/login">Go to Login</Link>
+      <br></br>
+      <Link to="/register">Go to Register</Link>
+    </>
   );
+
 }
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Register() {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleRegister = async (e: React.SubmitEvent) => {
+        e.preventDefault();
+
+        const res = await fetch("http://localhost:4000/users/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify({ email, name, password }),
+        });
+
+        if (res.ok) {
+            navigate("/dashboard");
+        } else {
+            const data = await res.json();
+            setError(data.error);
+        }
+    };
+
     return (
-        <>
-            <h1>what u registering</h1>
-        </> 
+        <form onSubmit={handleRegister}>
+            <input
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                placeholder="Email"
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            <input 
+                type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                placeholder="Name"
+            />
+            {error && <p>{error}</p>}
+            <button type="submit">Register!</button>
+        </form>
     )
 }
