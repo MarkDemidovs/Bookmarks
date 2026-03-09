5db7c46331d2df1a4befb93fe5c26412c9c48f31
MarkDemidovs <markdemidovs@proton.me>
2026-03-09 18:56:08 +0200

    client proper bookmark formatting

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,55 +3,55 @@ import type { bookmarkType } from "../types/bookmark";
 import { getAllBookmarks, createBookmark } from "../api/bookmarks";
 
 export default function Dashboard() {
-    const [bookmarks, setBookmarks] = useState<bookmarkType[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [url, setUrl] = useState("");
-    const [title, setTitle] = useState("");
+  const [bookmarks, setBookmarks] = useState<bookmarkType[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState("");
+  const [title, setTitle] = useState("");
 
-    useEffect(() => {
-        async function load() {
-            try {
-                const data = await getAllBookmarks();
-                setBookmarks(data);
-            } catch (err) {
-                
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        load();
-    }, [])
-
-
-    async function createBookmarkFunction(url: string, title: string) {
+  useEffect(() => {
+    async function load() {
       try {
-        const data = await createBookmark(url, title);
-        setBookmarks(prev => [...prev, data]);
+        const data = await getAllBookmarks();
+        setBookmarks(data);
       } catch (err) {
-        console.log(err);
+
+      } finally {
+        setLoading(false);
       }
     }
-    if (loading) return <div>Loading...</div>;
 
-    return (
-      <>
+    load();
+  }, [])
+
+
+  async function createBookmarkFunction(url: string, title: string) {
+    try {
+      const data = await createBookmark(url, title);
+      setBookmarks(prev => [...prev, data]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <>
       <form>
         <input value={url} placeholder="Enter new URL" onChange={e => setUrl(e.target.value)}></input>
         <input value={title} placeholder="Enter new title" onChange={e => setTitle(e.target.value)}></input>
         <button onClick={() => createBookmarkFunction(url, title)}>Create</button>
       </form>
-      <div className="gap-6 flex flex-wrap">
+      <div className="gap-6 flex flex-wrap flex-col">
         {bookmarks.map(bookmark => (
           <div>
             <p>
-                {bookmark.title}
-                {bookmark.url}
+              <a href={bookmark.url}>{bookmark.title}</a>
+
             </p>
           </div>
         ))}
       </div>
 
-      </>
-    )
+    </>
+  )
 }
